e156c51014251fbbb3ecbf2544da4917baad10fb
fabio231212 <ramirezrojasfabio@gmail.com>
2023-07-10 23:50:04 -0600

    cambios imagenes

--- server/prisma/seeds/imagenProducto.seed.ts
+++ server/prisma/seeds/imagenProducto.seed.ts
@@ -1,41 +1,40 @@
-import { producto } from "./producto.seed";
+import { producto } from './producto.seed';
 
 export const imagenProducto = [
   {
-    URL: "https://landerfit.com/wp-content/uploads/2022/12/Creatine.jpg",
+    URL: 'https://landerfit.com/wp-content/uploads/2022/12/Creatine.jpg',
     productoId: 1,
   },
   {
-    URL: "https://vitalikecr.com/wp-content/uploads/2022/12/creatina60_nutrabio.jpg",
+    URL: 'https://vitalikecr.com/wp-content/uploads/2022/12/creatina60_nutrabio.jpg',
     productoId: 2,
   },
   {
-    URL: "https://boutiquegimnasio.com/cdn/shop/products/61YYmlSTuHL._AC_SY606_750x.jpg?v=1670520300",
+    URL: 'https://crsuplementos.net/cdn/shop/products/IMG_6836_300x300.png?v=1677864159',
     productoId: 3,
   },
   {
-    URL: "https://boutiquegimnasio.com/cdn/shop/products/61lgprKutkL._AC_SX569__1_750x.jpg?v=1672781381",
+    URL: 'https://cdn.shopify.com/s/files/1/0021/9136/0084/products/image_2022-02-03_61fbd5cdf3dce_1024x1024.jpg?v=1656539452',
     productoId: 4,
   },
   {
-    URL: "https://vitalikecr.com/wp-content/uploads/2022/12/caseina_fresa.png",
+    URL: 'https://vitalikecr.com/wp-content/uploads/2022/12/caseina_fresa.png',
     productoId: 5,
   },
   {
-    URL: "https://walmartcr.vtexassets.com/arquivos/ids/408966-1200-900?v=638210794142330000&width=1200&height=900&aspect=true",
+    URL: 'https://walmartcr.vtexassets.com/arquivos/ids/408966-1200-900?v=638210794142330000&width=1200&height=900&aspect=true',
     productoId: 6,
   },
   {
-    URL: "https://boutiquegimnasio.com/cdn/shop/products/MUSCLETECH_PLATINUM_VITAMIN_90_CAPS._750x.png?v=1625274262",
+    URL: 'https://boutiquegimnasio.com/cdn/shop/products/MUSCLETECH_PLATINUM_VITAMIN_90_CAPS._750x.png?v=1625274262',
     productoId: 7,
   },
   {
-    URL: "https://vitalikecr.com/wp-content/uploads/2022/12/betalanina_nutrabio.jpg",
+    URL: 'https://vitalikecr.com/wp-content/uploads/2022/12/betalanina_nutrabio.jpg',
     productoId: 8,
   },
   {
-    URL: "https://landerfit.b-cdn.net/wp-content/uploads/2022/12/MULTIPHASE.jpg",
+    URL: 'https://landerfit.b-cdn.net/wp-content/uploads/2022/12/MULTIPHASE.jpg',
     productoId: 9,
   },
-
 ];
