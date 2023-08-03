9853864bc5b2e672bbb7519ac50326241772d7b4
fabio231212 <ramirezrojasfabio@gmail.com>
2023-08-02 23:06:48 -0600

    arreglos avance 4

--- server/core/presentation/routes/producto_routes.ts
+++ server/core/presentation/routes/producto_routes.ts
@@ -4,25 +4,25 @@ import { ProductoController } from '../controllers/ProductoController';
 import { verifyToken } from './authMiddleware';
 import { Router } from "express";
 // ES REQUERIDO PARA PODER HACER LAS GESTION DE IMAGENES EN LA RUTA DE PRODUCTOS
-import ImageUploader from  "../controllers/storageController";
+import ImageUploader from "../controllers/storageController";
 
 const productoRoute = Router();
 
 const productoController = new ProductoController();
 
 
-productoRoute.get('/', verifyToken, productoController.getProductos);
+productoRoute.get('/', productoController.getProductos);
 productoRoute.get('/:id', productoController.getProductoById);
-productoRoute.get('/idVendedor/:idVendedor',verifyToken,productoController.getProductosByIdVendedor
+productoRoute.get('/idVendedor/:idVendedor', verifyToken, productoController.getProductosByIdVendedor
 );
-productoRoute.get('/categoria/:idCategoria',verifyToken,productoController.getProductsByCategory
+productoRoute.get('/categoria/:idCategoria', productoController.getProductsByCategory
 );
 
 
 //Se utiliza la función 'upload.array' para procesar hasta 5 archivos con el nombre 'imagenes' adjuntados a la solicitud
 //ESTO SOLO SE PUEDE HACER GRACIAS AL USO DE MULTER, ESTO LLAMA AL CREATE PRODUCT Y A SU VEZ AL STORAGE CONTROLLER QUE MANEJA LA GESTION DE IMAGENES
-productoRoute.post('/',ImageUploader.upload.array('imagenes', 5),productoController.createProduct);
-productoRoute.put('/:id', ImageUploader.upload.array('imagenes', 5) ,productoController.editProduct);
+productoRoute.post('/', ImageUploader.upload.array('imagenes', 5), productoController.createProduct);
+productoRoute.put('/:id', ImageUploader.upload.array('imagenes', 5), productoController.editProduct);
 
 
 export default productoRoute;
