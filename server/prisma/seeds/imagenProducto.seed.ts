79307eefb3959d67d8169ba3756592e0f596a4e0
fabio231212 <ramirezrojasfabio@gmail.com>
2023-08-16 12:07:50 -0600

    imagen producto modificado

--- server/prisma/seeds/imagenProducto.seed.ts
+++ server/prisma/seeds/imagenProducto.seed.ts
@@ -4,39 +4,39 @@ import { producto } from './producto.seed';
 
 export const imagenProducto = [
   {
-    imgUrl: "https://landerfit.com/wp-content/uploads/2022/12/Creatine.jpg",
+    imgUrl: "http://localhost:8000/public/Creatine.jpg",
     productoId: 1,
   },
   {
-    imgUrl: "https://vitalikecr.com/wp-content/uploads/2022/12/creatina60_nutrabio.jpg",
+    imgUrl: "http://localhost:8000/public/creatina60_nutrabio.jpg",
     productoId: 2,
   },
   {
-    imgUrl: "https://boutiquegimnasio.com/cdn/shop/products/61YYmlSTuHL._AC_SY606_750x.jpg?v=1670520300",
+    imgUrl: "http://localhost:8000/public/61YYmlSTuHL._AC_SY606_750x.jpg?v=1670520300",
     productoId: 3,
   },
   {
-    imgUrl: "https://boutiquegimnasio.com/cdn/shop/products/61lgprKutkL._AC_SX569__1_750x.jpg?v=1672781381",
+    imgUrl: "http://localhost:8000/public/61lgprKutkL._AC_SX569__1_750x.jpg?v=1672781381",
     productoId: 4,
   },
   {
-    imgUrl: "https://vitalikecr.com/wp-content/uploads/2022/12/caseina_fresa.png",
+    imgUrl: "http://localhost:8000/public/caseina_fresa.png",
     productoId: 5,
   },
   {
-    imgUrl: "https://walmartcr.vtexassets.com/arquivos/ids/408966-1200-900?v=638210794142330000&width=1200&height=900&aspect=true",
+    imgUrl: "http://localhost:8000/public/iso100.png",
     productoId: 6,
   },
   {
-    imgUrl: "https://boutiquegimnasio.com/cdn/shop/products/MUSCLETECH_PLATINUM_VITAMIN_90_CAPS._750x.png?v=1625274262",
+    imgUrl: "http://localhost:8000/public/nutrabioCreatine.png",
     productoId: 7,
   },
   {
-    imgUrl: "https://vitalikecr.com/wp-content/uploads/2022/12/betalanina_nutrabio.jpg",
+    imgUrl: "http://localhost:8000/public/betalanina_nutrabio.jpg",
     productoId: 8,
   },
   {
-    imgUrl: "https://landerfit.b-cdn.net/wp-content/uploads/2022/12/MULTIPHASE.jpg",
+    imgUrl: "http://localhost:8000/public/MULTIPHASE.jpg",
     productoId: 9,
   },
 ];
